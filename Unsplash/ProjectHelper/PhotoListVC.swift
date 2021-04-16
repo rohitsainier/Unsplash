ea02933a1c94e6239a426d30252fd6c86a6b7a8b
rohitsainier <rohitsainier@gmail.com>
2021-04-16 21:34:45 +0530

    PhotoList Module required builder  file added

---
+++ Unsplash/ProjectHelper/PhotoListVC.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoListVC.swift
+//  Unsplash
+//
+//  Created by Rohit Saini on 16/04/21.
+//
+
+import UIKit
+
+class PhotoListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+
+}
---
+++ Unsplash/ProjectHelper/PhotoListViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  PhotoListViewModel.swift
+//  Unsplash
+//
+//  Created by App Knit on 16/04/21.
+//
+
+import Foundation
+
+
