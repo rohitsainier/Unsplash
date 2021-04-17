24f2117ec5c8d3d391d6ac6cf04d26236d3f4e8e
rohitsainier <rohitsainier@gmail.com>
2021-04-17 23:01:28 +0530

    PhotoList VC UI structure added

--- Unsplash/View/PhotoListVC.swift
+++ Unsplash/View/PhotoListVC.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class PhotoListVC: UIViewController {
 
+    
+    //OUTLETS
+    @IBOutlet weak var topImageView: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
---
+++ Unsplash/View/Xib's/PhotoCell.swift
@@ -0,0 +1,23 @@
+//
+//  PhotoCell.swift
+//  Unsplash
+//
+//  Created by App Knit on 17/04/21.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
