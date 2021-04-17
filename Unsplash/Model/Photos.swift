b6833f1e731343b5cefc5ee7734312b4947301e3
rohitsainier <rohitsainier@gmail.com>
2021-04-18 00:30:45 +0530

    default values added if keys are not present in response

--- Unsplash/Model/Photos.swift
+++ Unsplash/Model/Photos.swift
@@ -18,26 +18,53 @@ struct Photo: Codable {
         case photoDescription = "description"
         case user, urls
     }
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = try values.decodeIfPresent(String.self, forKey: .id) ?? DecodeDefaultValues.Empty.string
+        photoDescription = try values.decodeIfPresent(String.self, forKey: .photoDescription) ?? DecodeDefaultValues.Empty.string
+        user = try values.decodeIfPresent(User.self, forKey: .user) ?? nil
+        urls = try values.decodeIfPresent(Urls.self, forKey: .urls) ?? nil
+    }
+    
 }
 
 // MARK: - Urls
 struct Urls: Codable {
     let raw, full, regular, small: String
     let thumb: String
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        raw = try values.decodeIfPresent(String.self, forKey: .raw) ?? DecodeDefaultValues.Empty.string
+        full = try values.decodeIfPresent(String.self, forKey: .full) ?? DecodeDefaultValues.Empty.string
+        regular = try values.decodeIfPresent(String.self, forKey: .regular) ?? DecodeDefaultValues.Empty.string
+        small = try values.decodeIfPresent(String.self, forKey: .small) ?? DecodeDefaultValues.Empty.string
+        thumb = try values.decodeIfPresent(String.self, forKey: .thumb) ?? DecodeDefaultValues.Empty.string
+    }
 }
 
 // MARK: - User
 struct User: Codable {
     let id, name, location: String
-    let profileImage: ProfileImage
+    let profileImage: ProfileImage?
 
     enum CodingKeys: String, CodingKey {
         case id, name, location
         case profileImage = "profile_image"
+    }
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = try values.decodeIfPresent(String.self, forKey: .id) ?? DecodeDefaultValues.Empty.string
+        name = try values.decodeIfPresent(String.self, forKey: .name) ?? DecodeDefaultValues.Empty.string
+        location = try values.decodeIfPresent(String.self, forKey: .location) ?? DecodeDefaultValues.Empty.string
+        profileImage = try values.decodeIfPresent(ProfileImage.self, forKey: .profileImage) ?? nil
     }
 }
 
 // MARK: - ProfileImage
 struct ProfileImage: Codable {
     let small: String
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        small = try values.decodeIfPresent(String.self, forKey: .small) ?? DecodeDefaultValues.Empty.string
+    }
 }
--- Unsplash/ProjectHelper/AppEnum.swift
+++ Unsplash/ProjectHelper/AppEnum.swift
@@ -11,3 +11,14 @@ import Foundation
 enum TABLE_VIEW_CELL:String{
     case PhotoCell
 }
+
+//MARK:- DocumentDefaultValues
+struct DecodeDefaultValues{
+    struct Empty{
+        static let string =  ""
+        static let int =  0
+        static let bool = false
+        static let double = 0.0
+    }
+}
+
