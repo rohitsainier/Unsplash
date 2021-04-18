c7f7558c238096dd0393ea0a8bfb529ce3c5a9de
rohitsainier <rohitsainier@gmail.com>
2021-04-18 10:24:03 +0530

    profile pic rounded border added

--- Unsplash/View/Xib's/PhotoCell.swift
+++ Unsplash/View/Xib's/PhotoCell.swift
@@ -16,12 +16,17 @@ class PhotoCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        configUI()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    private func configUI(){
+        profilePic.layer.borderWidth = 2
+        profilePic.layer.borderColor = UIColor.white.cgColor
     }
     
 }
